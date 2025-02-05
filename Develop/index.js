2297a2bd5dacfbc32a2b02d3df68796654cde5b1
Hunter <hunterwest94@rocketmail.com>
2025-02-04 22:28:59 -0500

    committing for the hw

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+import inquirer from 'inquirer';
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js"
+import Choices from 'inquirer/lib/objects/choices.js';
 // TODO: Create an array of questions for user input
-const questions = [];
+//create questions for title!, Description!, Table of Contents!, Installation, Usage
+//License, Contributing, Tests, and Questions
+const questions = [ {
+          type: 'imput',
+          message: 'title of my project.',
+          name: 'title',
+        },
+        {
+          type:'imput',
+          message:'Please enter a description for the application.',
+          name:'des',
+        },
+        {
+          type:'imput',
+          message:'Please enter an install guide for the application.',
+          name:'install',
+        },
+        {
+          type:'imput',
+          message:'Please enter a usage guide for the application.',
+          name:'usage',
+        }, 
+        {
+          type:'imput',
+          message:'Please enter test guides for the application.',
+          name:'tests',
+        },
+        {
+          type:'list',
+          message:'You got a licinse for that thing?',
+          choices: ['MIT', 'Apache 2.0', 'ISC',],
+          name:'license',
+        },
+        {
+          type:'imput',
+          message:'contribution guidelines',
+          name:'contributing',
+        },
+        {
+          type:'imput',
+          message:'Whats your Name for those who have questions?',
+          name:'githubName',
+        },
+        {
+          type:'imput',
+          message:'Whats your email for those who have questions?',
+          name:'email',  
+        }
+        ,];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README Has Been Made!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {if (license === 'MIT')
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  else if (license ==='Apache 2.0')
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  else if (license ==='ISC')
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+  else 
+  return;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {if (license === 'MIT')
+  return `(https://opensource.org/licenses/MIT)`;
+  else if (license ==='Apache 2.0')
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  else if (license ==='ISC')
+    return `(https://opensource.org/licenses/ISC)`;
+  else 
+  return;}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {if (license){return `${license}`;}
+else {return};
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.des}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## installation
+  ${data.install}
+
+  ## usage
+  ${data.usage}
+
+  ## license
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ##contributing
+  ${data.contributing}
+
+  ## tests
+  ${data.tests}
+
+  ##questions
+  Any quebbins? Hit me up yo! ${data.githubName} at ${data.email}
 
 `;
 }
